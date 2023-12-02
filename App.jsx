4479685eb0407eafd399cda8ee25dbe9932dffce
kulsumsamgit <137611398+kulsumsamgit@users.noreply.github.com>
2023-12-02 18:04:39 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+  const addToList = () => {
+    let tempArr = list;
+    tempArr.push(value);
+    setList(tempArr);
+    setValue("");
+  };
+  const deleteItem = (index) => {
+    let temp = list.filter((item, i) => i !== index);
+       setList(temp);
+     };
+     return (
+       <div className="App">
+           <fieldset>
+           <h>Add Product to List</h><br></br>
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+    <button onClick={addToList}> Click to Add </button><br></br><br></br>
+    <h>Product Catalog</h><br></br>
+    <ol>
+        {list.map((item, i) => <li onClick={() => deleteItem(i)}>{item} </li>)}
+    </ol>
+    <h>Click on Product to Delete</h><br></br>
+    </fieldset></div>
+  );
+     }
+     export default App
